913d22f7e06d53b43d22b0751dcb396632e3a35b
whahidul12 <mdislamwhahid@gmail.com>
2026-03-27 23:56:04 +0600

    Adjust dashboard panel widget widths
    Update the left and right dashboard panel widths to use responsive
    classes. This ensures the layout adjusts correctly across different
    screen sizes, specifically for medium devices and up.

--- shared/widgets/dashboard/dashboardPanelWidgets.tsx
+++ shared/widgets/dashboard/dashboardPanelWidgets.tsx
@@ -4,7 +4,7 @@ export default function DashboardPanelWidgets() {
   return (
     <div className="flex flex-col justify-between p-4 md:flex-row">
       {/*Left Side DashBoard Panel*/}
-      <div className="w-2/3 lg:w-full">
+      <div className="md:1/2 w-full lg:w-2/3">
         {/*User Card*/}
         <UserCard userType="students"></UserCard>
         <UserCard userType="teachers"></UserCard>
@@ -12,13 +12,7 @@ export default function DashboardPanelWidgets() {
         <UserCard userType="stuff"></UserCard>
       </div>
       {/*Right Side DashBoard Panel*/}
-      <div className="w-1/3 lg:w-full">
-        {" "}
-        <UserCard userType="students"></UserCard>
-        <UserCard userType="teachers"></UserCard>
-        <UserCard userType="parents"></UserCard>
-        <UserCard userType="stuff"></UserCard>
-      </div>
+      <div className="md:1/2 w-full lg:w-1/3">k</div>
     </div>
   );
 }
