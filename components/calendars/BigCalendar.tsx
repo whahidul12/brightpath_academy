155412921273eb1dab28d23b556fc12f5d166413
whahidul12 <mdislamwhahid@gmail.com>
2026-03-30 23:04:24 +0600

    Refactor BigCalendar to hide weekends
    This commit introduces a new CSS file to hide the weekend days (Friday and Saturday) from the BigCalendar
    component. It also adjusts the component's props and logic to ensure the remaining days fill the available width
    correctly. The calendar events are also updated to reflect the new date range.

--- components/calendars/BigCalendar.tsx
+++ components/calendars/BigCalendar.tsx
@@ -1,20 +1,52 @@
 "use client";
-import { Calendar, momentLocalizer } from "react-big-calendar";
+import { Calendar, momentLocalizer, View, Views } from "react-big-calendar";
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import { calendarEvents } from "@/constants/data";
+import { useMemo, useState } from "react";
+import "./BigCalendar.css";
+
+moment.updateLocale("en", {
+  week: {
+    dow: 0,
+    doy: 4,
+  },
+});
 
 const localizer = momentLocalizer(moment);
 
-export const BigCalendar = (props) => {
+export const BigCalendar = () => {
+  const [view, setView] = useState<View>(Views.MONTH);
+
+  const handleViewChange = (newView: View) => {
+    setView(newView);
+  };
+
+  const viewsList = useMemo(() => ["month", "week", "day"] as View[], []);
+
   return (
-    <div>
+    <div style={{ height: "95vh" }}>
       <Calendar
         localizer={localizer}
         events={calendarEvents}
         startAccessor="start"
         endAccessor="end"
-        style={{ height: 500 }}
+        views={viewsList}
+        view={view}
+        onView={handleViewChange}
+        style={{ height: "100%" }}
+        dayPropGetter={(date) => {
+          const day = date.getDay();
+          if (day === 5 || day === 6) {
+            return {
+              className: "hidden-day",
+              style: { display: "none" },
+            };
+          }
+          return {};
+        }}
+        min={new Date(2026, 0, 1, 8, 0, 0)}
+        max={new Date(2026, 0, 1, 17, 0, 0)}
       />
     </div>
   );
